e08f14f94bfe2f1f3e7929379c84a153067db629
mune-kyun <rzknurfaizi@gmail.com>
2023-11-18 19:02:58 +0700

    refactor

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 import * as vscode from "vscode";
 
-import convertService from "./convert";
+import changeWord from "./changeWord";
+import convertService from "./services/convert";
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "dev-bit-help" is now active!');
@@ -8,42 +9,14 @@ export function activate(context: vscode.ExtensionContext) {
   // Camel to Kebab
   context.subscriptions.push(
     vscode.commands.registerCommand("dev-bit-help.camelToKebab", () => {
-      const editor = vscode.window.activeTextEditor;
-
-      if (editor) {
-        const document = editor.document;
-        const selection = editor.selection;
-
-        // Convert the word
-        const word = document.getText(selection);
-        const kebabWord = convertService.camelToKebab(word);
-
-        // Change the text
-        editor.edit((editBuilder) => {
-          editBuilder.replace(selection, kebabWord);
-        });
-      }
+      changeWord(convertService.camelToKebab);
     })
   );
 
   // Kebab to Camel
   context.subscriptions.push(
     vscode.commands.registerCommand("dev-bit-help.kebabToCamel", () => {
-      const editor = vscode.window.activeTextEditor;
-
-      if (editor) {
-        const document = editor.document;
-        const selection = editor.selection;
-
-        // Convert the word
-        const word = document.getText(selection);
-        const kebabWord = convertService.kebabToCamel(word);
-
-        // Change the text
-        editor.edit((editBuilder) => {
-          editBuilder.replace(selection, kebabWord);
-        });
-      }
+      changeWord(convertService.kebabToCamel);
     })
   );
 }
