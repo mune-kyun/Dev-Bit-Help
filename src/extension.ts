f1e6a3d471d452d55a8a78af3da7ca55b56fb57d
mune-kyun <rzknurfaizi@gmail.com>
2023-11-18 18:36:14 +0700

    camel to kebab and vice versa

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,51 @@
 import * as vscode from "vscode";
 
+import convertService from "./convert";
+
 export function activate(context: vscode.ExtensionContext) {
   console.log(
     'Congratulations, your extension "vscode-camel-kebab" is now active!'
   );
 
+  // Camel to Kebab
   context.subscriptions.push(
     vscode.commands.registerCommand("vscode-camel-kebab.camelToKebab", () => {
-      vscode.window.showInformationMessage(
-        "Hello World from vscode-camel-kebab!"
-      );
-      console.log("called");
+      const editor = vscode.window.activeTextEditor;
+
+      if (editor) {
+        const document = editor.document;
+        const selection = editor.selection;
+
+        // Convert the word
+        const word = document.getText(selection);
+        const kebabWord = convertService.camelToKebab(word);
+
+        // Change the text
+        editor.edit((editBuilder) => {
+          editBuilder.replace(selection, kebabWord);
+        });
+      }
+    })
+  );
+
+  // Kebab to Camel
+  context.subscriptions.push(
+    vscode.commands.registerCommand("vscode-camel-kebab.kebabToCamel", () => {
+      const editor = vscode.window.activeTextEditor;
+
+      if (editor) {
+        const document = editor.document;
+        const selection = editor.selection;
+
+        // Convert the word
+        const word = document.getText(selection);
+        const kebabWord = convertService.kebabToCamel(word);
+
+        // Change the text
+        editor.edit((editBuilder) => {
+          editBuilder.replace(selection, kebabWord);
+        });
+      }
     })
   );
 }
